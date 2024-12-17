4dcb95a93f13442039a55800e1ea1e26337f94d4
aman.dubey <aman.dubey@atmecs.com>
2024-12-18 00:00:05 +0530

    added graph

---
+++ dataStructures/graph.js
@@ -0,0 +1,47 @@
+class Graph {
+    constructor() {
+        this.adjacencyList = {};
+    }
+
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    }
+
+    addEdge(v1, v2) {
+        if (!this.adjacencyList[v1]) this.addVertex(v1);
+        if (!this.adjacencyList[v2]) this.addVertex(v2);
+
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    }
+
+    removeEdge(v1, v2) {
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2);
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v !== v1);
+    }
+
+    removeVertex(vertex) {
+        while (this.adjacencyList[vertex].length) {
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+        delete this.adjacencyList[vertex];
+    }
+
+    display() {
+        console.log(this.adjacencyList);
+    }
+}
+
+const graph = new Graph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "D");
+graph.addEdge("C", "D");
+graph.removeEdge("A", "C");
+graph.removeVertex("D");
+graph.display();
